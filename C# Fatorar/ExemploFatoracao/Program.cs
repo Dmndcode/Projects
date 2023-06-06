c1f5bf4bb266a00a7d7a2fd03dc4f02ab26debac
Rafael Guindani <119433897+Dmndcode@users.noreply.github.com>
2023-06-06 17:57:11 -0300

    Add files via upload

---
+++ C# Fatorar/ExemploFatoracao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        BigInteger fatorial = 1;
+
+        Console.WriteLine("Insira um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int i = numero;
+        while (i >= 1) { fatorial *= i; i--; }
+        string escreva = "o fatorial de " + numero + " é " + fatorial;
+        Console.WriteLine(escreva);
+    }
+}
