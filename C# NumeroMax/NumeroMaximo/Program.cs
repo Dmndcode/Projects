ce886663689deddbef1a1d8951afe806c94b6de2
Rafael Guindani <119433897+Dmndcode@users.noreply.github.com>
2023-06-06 17:58:04 -0300

    Add files via upload

---
+++ C# NumeroMax/NumeroMaximo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Escreva uma série de números separados por virgula");
+        string input = Console.ReadLine() ?? string.Empty;
+        String[]? numbers = input?.Split(',');
+        int max = int.MinValue;
+
+        foreach (String number in numbers ?? Array.Empty<string>())
+        {
+            if (int.TryParse(number?.Trim(), out int currentNmbr)) 
+            {
+                if (currentNmbr > max) { max = currentNmbr; }
+            }
+            else { Console.WriteLine("Entrada inválida: " + number); break; }
+            
+        }
+        Console.WriteLine("O maior numero da série é: " + max);
+    }
+}
