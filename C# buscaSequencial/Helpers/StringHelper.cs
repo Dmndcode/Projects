78ff7919f3ddcf8c6aec054436b365021e5b2db1
Rafael Guindani <119433897+Dmndcode@users.noreply.github.com>
2023-08-26 10:57:24 -0300

    Add files via upload

---
+++ C# buscaSequencial/Helpers/StringHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buscaSequencial.Helpers
+{
+    public static class StringHelper
+    {
+        public static string RemoverAcentos(string texto)
+        {
+            string normalizedString = texto.Normalize(NormalizationForm.FormD);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (char c in normalizedString)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ C# buscaSequencial/Program.cs
@@ -0,0 +1,50 @@
+﻿using buscaSequencial.Models;
+using buscaSequencial.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace buscaSequencial
+{
+    class Program
+    {
+        static bool BuscaCafe(TipoDeCafe cafe, string termoBusca)
+        {
+            string nomeSemAcentos = StringHelper.RemoverAcentos(cafe.Nome.ToLower());
+            string termoSemAcentos = StringHelper.RemoverAcentos(termoBusca.ToLower());
+
+            return nomeSemAcentos.Contains(termoSemAcentos);
+        }
+
+        static void Main(string[] args) 
+        {
+            List<TipoDeCafe> tiposDeCafes = new List<TipoDeCafe> {
+                new TipoDeCafe { Nome = "Café Arábica", Descricao = "Um café de sabor suave e aromático, cultivado em regiões de alta altitude." },
+                new TipoDeCafe { Nome = "Café Robusta", Descricao = "Conhecido por sua robustez e sabor intenso, frequentemente usado em blends de espresso." },
+                new TipoDeCafe { Nome = "Café Kona", Descricao = "Originário do Havaí, este café possui notas frutadas e é cultivado em solos vulcânicos." },
+                new TipoDeCafe { Nome = "Café Geisha", Descricao = "Uma variedade de café especial conhecida por seus sabores florais e complexidade." }
+            };
+
+            Console.Write("Insira o nome de um tipo de café: ");
+            string nomeBuscado = Console.ReadLine().Trim().ToLower();
+
+            bool encontrado = false;
+            foreach (var cafe in tiposDeCafes)
+            {
+                if (BuscaCafe(cafe, nomeBuscado))
+                {
+                    Console.WriteLine($"Tipo de Café: {cafe.Nome}");
+                    Console.WriteLine($"Descrição: {cafe.Descricao}");
+                    encontrado = true;
+                }
+            }
+            if (!encontrado)
+            {
+                Console.WriteLine("Tipo de café não encontrado.");
+            }
+        }
+    }
+}
